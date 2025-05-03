d6197d372b2ee242054fc0c835c04ee4b79b2010
Bhushan280 <bhushanchouhan25@gmail.com>
2025-05-04 03:03:44 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
+// In App.js, change this import:
+import PricingOptimization from './pages/PricingOptimization';
+
+// From your code, it should look like:
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import ProductList from './pages/ProductList';
 import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<ProductList />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<ProductList />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/pricing' element={<PricingOptimization />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
